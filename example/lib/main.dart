62f11e10a60fca473b26889341e0d1bb9d4b975b
Ernest <66331153+Weav3r@users.noreply.github.com>
2025-05-24 09:19:49 +0000

    save last good known b4 real example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,23 +1,23 @@
-import 'package:aw_router/aw_router.dart';
+import 'package:aw_router/aw_router.dart' as awr;
 import 'package:awr_example/users.dart';
 
 Future<dynamic> main(final context) async {
   try {
     final log = context.log;
-    final router = Router(context);
+    final router = awr.Router(context);
 
-    foo(RequestHandler handler) {
-      return (Request request) async {
+    foo(awr.RequestHandler handler) {
+      return (awr.Request request) async {
         final modReq =
             request.copyWith(context: {...request.context, 'foo': 'Hi'});
         log('foo ${modReq.context}');
         final r = await handler(modReq);
         return r.modify(body: "foo(${r.body})");
-        // return Response().modify(body: 'Sooo()');
+        // return awr.Response().modify(body: 'Sooo()');
       };
     }
 
-    Middleware modifyMiddleware() {
+    awr.Middleware modifyMiddleware() {
       return (handler) {
         return (request) async {
           //Modify original request
@@ -28,14 +28,14 @@ Future<dynamic> main(final context) async {
           //Avoid using print. This is here for demonstration purposes
           print('Modified request: ${modifiedReq.context}');
 
-          final Response r = await handler(modifiedReq);
+          final awr.Response r = await handler(modifiedReq);
           //Modify response from handler
           return r.modify(body: "Modfied response body(${r.body})");
         };
       };
     }
 
-    Middleware s1() {
+    awr.Middleware s1() {
       return (handler) {
         return (request) async {
           final modReq =
@@ -43,31 +43,31 @@ Future<dynamic> main(final context) async {
           log('s1 ${modReq.context}');
           final r = await handler(modReq);
           return r.modify(body: "s1(${r.body})");
-          // return Response().modify(body: 'Sooo()');
+          // return awr.Response().modify(body: 'Sooo()');
         };
       };
     }
 
-    router.get('/', (Request req) {
-      // if (res case Response _) {
+    router.get('/', (awr.Request req) {
+      // if (res case awr.Response _) {
       // res.modify(body: "You got users root with modify()");
       // }
-      log("Request runtime type: $req");
-      final res = Response().modify(body: "Index page got", code: 200);
+      log("awr.Request runtime type: $req");
+      final res = awr.Response().modify(body: "Index page got", code: 200);
       log("CURRENT RESPONSE ${res}");
       return res;
     });
 
-    router.get('/redirect', (Request req) {
-      // if (res case Response _) {
+    router.get('/redirect', (awr.Request req) {
+      // if (res case awr.Response _) {
       // res.modify(body: "You got users root with modify()");
       // }
-      log("Request runtime type: $req");
+      log("awr.Request runtime type: $req");
       req.headers['redirectUrl'] = true;
-      // final res = Response().modify(body: "Index page got", code: 200);
-      final res = Response(
+      // final res = awr.Response().modify(body: "Index page got", code: 200);
+      final res = awr.Response(
           body: '', code: 301, headers: {'Location': 'https://exmaple.com'});
-      // final res = Response(
+      // final res = awr.Response(
       //     body: 'https://exmaple.com',
       //     code: 200,
       //     headers: {'redirectUrl': true});
@@ -79,13 +79,14 @@ Future<dynamic> main(final context) async {
 
     router.all('/<chaff|.*>', middlewares: [modifyMiddleware(), s1(), foo],
         (req) async {
-      return Response(body: "[AWR] Sorry, I'm Default modify(${req.context})");
-      // return Response(body: "Sorry, I'm Default modify()", code: 404);
+      return awr.Response(
+          body: "[AWR] Sorry, I'm Default modify(${req.context})");
+      // return awr.Response(body: "Sorry, I'm Default modify()", code: 404);
     });
 
-    final Response res = await Pipeline()
+    final awr.Response res = await awr.Pipeline()
         // .addMiddleware(sooo())
-        .handler(router.call)(Request.parse(context.req));
+        .handler(router.call)(awr.Request.parse(context.req));
     return res.runtimeResponse(context.res);
 
     // return res.modify(body: 'Helooo me').resBody(context.res);
